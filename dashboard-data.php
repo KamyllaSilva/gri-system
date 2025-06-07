99c44ef4627114295659cd2597d2a88abb5169c2
KamyllaSilva <silvakamylla670@gmail.com>
2025-06-06 23:12:47 -0300

    db

--- dashboard-data.php
+++ dashboard-data.php
@@ -1,8 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 session_start();
-ini_set('display_errors', 0);
-error_reporting(0);
-
 header('Content-Type: application/json');
 
 // Verifica se usuário está logado
