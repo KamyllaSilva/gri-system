bdbfb0d2274e2fbee0c9e7ee620aac289341e817
KamyllaSilva <silvakamylla670@gmail.com>
2025-06-14 13:49:43 -0300

    14.06

--- includes/mailer.php
+++ includes/mailer.php
@@ -1,9 +1,5 @@
 <?php
-require_once __DIR__ . '/phpmailer/src/PHPMailer.php';
-require_once __DIR__ . '/phpmailer/src/SMTP.php';
-require_once __DIR__ . '/phpmailer/src/Exception.php';
-var_dump(file_exists(__DIR__ . '/phpmailer/src/PHPMailer.php'));
-
+require_once __DIR__ . '/../vendor/autoload.php'; // Ajuste o caminho para o seu arquivo autoload.php
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
@@ -12,11 +8,12 @@ function enviarCodigoRedefinicao(string $emailDestino, int $codigo): bool {
     $mail = new PHPMailer(true);
 
     try {
+        // Configurações do servidor SMTP (exemplo Gmail)
         $mail->isSMTP();
         $mail->Host       = 'smtp.gmail.com';
         $mail->SMTPAuth   = true;
-        $mail->Username   = 'seuemail@gmail.com';         // Seu e-mail
-        $mail->Password   = 'sua_senha_de_aplicativo';    // Senha de App do Gmail
+        $mail->Username   = 'seuemail@gmail.com';
+        $mail->Password   = 'sua_senha_de_aplicativo';
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         $mail->Port       = 587;
 
@@ -24,13 +21,13 @@ function enviarCodigoRedefinicao(string $emailDestino, int $codigo): bool {
         $mail->addAddress($emailDestino);
 
         $mail->isHTML(true);
-        $mail->Subject = 'Código de redefinição de senha';
-        $mail->Body    = "<p>Olá,</p><p>Seu código é: <strong>$codigo</strong></p>";
+        $mail->Subject = 'Redefinição de senha - Código de verificação';
+        $mail->Body    = "<p>Olá,</p><p>Seu código de redefinição de senha é: <strong>$codigo</strong></p>";
 
         $mail->send();
         return true;
     } catch (Exception $e) {
-        error_log("Erro ao enviar: " . $e->getMessage());
+        error_log("Erro ao enviar e-mail: {$mail->ErrorInfo}");
         return false;
     }
 }
