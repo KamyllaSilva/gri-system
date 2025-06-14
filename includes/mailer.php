eaa180bd239bbf2a96c0651a343112bb26f6f299
KamyllaSilva <silvakamylla670@gmail.com>
2025-06-14 13:46:43 -0300

    14.06

--- includes/mailer.php
+++ includes/mailer.php
@@ -2,6 +2,8 @@
 require_once __DIR__ . '/phpmailer/src/PHPMailer.php';
 require_once __DIR__ . '/phpmailer/src/SMTP.php';
 require_once __DIR__ . '/phpmailer/src/Exception.php';
+var_dump(file_exists(__DIR__ . '/phpmailer/src/PHPMailer.php'));
+
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
